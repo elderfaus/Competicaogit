9fd43a4cec2f44cc286425aabe7665c0261bf418
elder de andrade lorena <elderorama@gmail.com>
2019-11-21 00:10:21 -0300

    Redimensionado o Form2

--- WindowsFormsApp1/Form2.Designer.cs
+++ WindowsFormsApp1/Form2.Designer.cs
@@ -183,7 +183,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(822, 661);
+            this.ClientSize = new System.Drawing.Size(750, 661);
             this.Controls.Add(this.textBox16);
             this.Controls.Add(this.textBox15);
             this.Controls.Add(this.textBox14);
